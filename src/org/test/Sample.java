31e6efc554edcd56669b1b07c30ee3011529403f
sathishkumar786 <sam.pearl786@gmail.com>
2019-03-29 11:49:35 +0530

    This is my Sample Employee detials

---
+++ src/org/test/Sample.java
@@ -0,0 +1,31 @@
+package org.test;
+
+public class Sample {
+	 
+	public void empid()
+	{
+		System.out.println("Emp Id is: 27689");
+	}	
+	public void empname()
+	{
+		System.out.println("Emp Name: sathish kumar");
+	}
+public void empadd()
+{
+	System.out.println("Emp Add: 41,AVg Street cuddalore");
+	
+}	
+	public void empemail()
+	{
+		System.out.println("Emp Emailid: sam.pearl786@gmail.com");
+	}
+	public static void main(String[] args) {
+		Sample sc=new Sample();
+		sc.empid();
+		sc.empname();
+		sc.empadd();
+		sc.empemail();
+	}
+	}
+
+
